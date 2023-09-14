6a0ed9226663a86d87a5ea3ffa347633cc15aec7
Namu001 <108710666+Namu001@users.noreply.github.com>
2023-09-14 15:20:50 +0530

    Add files via upload

---
+++ Untitled3.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Python3 program to add two numbers
+num1 = 15
+num2 = 12
+
+# Adding two nos
+sum = num1 + num2
+
+# printing values
+print("Sum of", num1, "and", num2 , "is", sum)
+
+
+# In[ ]:
+
+
+
+
